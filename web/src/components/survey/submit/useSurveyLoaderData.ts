07a74c5e5223aabf82722d34e90272038400bbb2
NatSquared <nat.k.weiland@gmail.com>
2026-04-15 17:42:15 -0700

    Fix hook order error in useSurveyLoaderData.ts

--- web/src/components/survey/submit/useSurveyLoaderData.ts
+++ web/src/components/survey/submit/useSurveyLoaderData.ts
@@ -2,5 +2,8 @@ import { useRouteLoaderData } from 'react-router';
 import { SurveyLoaderData } from '../building/SurveyLoader';
 
 export const useSurveyLoaderData = () => {
-    return (useRouteLoaderData('survey') ?? useRouteLoaderData('public-survey')) as SurveyLoaderData;
+    const surveyLoaderData = useRouteLoaderData('survey');
+    const publicSurveyLoaderData = useRouteLoaderData('public-survey');
+
+    return (surveyLoaderData ?? publicSurveyLoaderData) as SurveyLoaderData;
 };
