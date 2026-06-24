483f4a203c263f609387fbadd6933a484d7d5365
NatSquared <nat.k.weiland@gmail.com>
2026-06-24 14:22:45 -0700

    Fix web CI/CD

--- web/tests/unit/components/survey/formioSetup.test.tsx
+++ web/tests/unit/components/survey/formioSetup.test.tsx
@@ -30,7 +30,7 @@ describe('setupFormio', () => {
     });
 
     it('registers met-formio and common-hosted-form-service components once', async () => {
-        const setupFormio = (await import('./setup')).default;
+        const setupFormio = (await import('components/Form/formio/setup')).default;
         const metFormio = (await import('met-formio')).default;
         const bcgovFormio = (await import('@bcgov/formio')).default;
         const { Formio } = await import('@formio/react');
@@ -44,7 +44,7 @@ describe('setupFormio', () => {
     });
 
     it('does not re-register components after initialization', async () => {
-        const setupFormio = (await import('./setup')).default;
+        const setupFormio = (await import('components/Form/formio/setup')).default;
 
         setupFormio();
         setupFormio();
