deec1ac83f7337b3ef0dc8b473faf552b1d0d52d
NatSquared <nat.k.weiland@gmail.com>
2026-06-23 17:22:10 -0700

    Import CHEFS components

--- web/src/components/Form/formio/setup.ts
+++ web/src/components/Form/formio/setup.ts
@@ -1,6 +1,7 @@
 // components/Form/formio/setup.ts
 import { Formio } from '@formio/react';
 import FormioComponents from 'met-formio';
+import BCGovFormioComponents from '@bcgov/formio';
 import 'components/Form/formio.scss';
 
 let initialized = false;
@@ -8,6 +9,7 @@ let initialized = false;
 export default function setupFormio() {
     if (initialized) return;
     Formio.use(FormioComponents);
+    Formio.use(BCGovFormioComponents);
     Formio.Utils.Evaluator.noeval = false;
     initialized = true;
 }
