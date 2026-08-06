f919e36a4c5857bc7377142417048fdc261d00cb
Jareth <110929259+jareth-whitney@users.noreply.github.com>
2026-08-06 15:38:06 -0700

    Feature/dep317: Updated pagination look and functionality, added engagement sort, added engagement count (#2918)
    * feature/dep316: New pagination system integrated.
    
    * feature/dep317: Added sort and engagement counts. Cleaned up code, inserted translation strings, used color variables from theme.
    
    * feature/dep317: Revised sort parsing logic, installed needed bootstrap dependency after merge.
    
    * feature/dep317: Code QL optimizations and card layout optimization.
    
    * feature/dep317: Fixed page display behaviour of pagination component by ditching MUI pagination and creating a custom component.
    
    * feature/dep317: Integrated requested review changes.

--- web/src/components/common/Input/Pagination.tsx
+++ web/src/components/common/Input/Pagination.tsx
@@ -1,86 +1,148 @@
 import React from 'react';
-import { Pagination as MuiPagination, PaginationItem, PaginationProps, styled } from '@mui/material';
-import { colors, elevations, globalFocusShadow } from '..';
+import { Grid2 as Grid, useMediaQuery, Theme } from '@mui/material';
+import { colors } from '..';
+import { Button } from './Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/pro-regular-svg-icons';
+import { BodyText } from '../Typography';
+import { useAppTranslation } from 'hooks';
 
-const PrimaryButtonStyles = {
-    boxShadow: elevations.default,
-    backgroundColor: colors.button.default.shade,
-    color: colors.type.inverted.primary,
-    '&:focus': {
-        backgroundColor: `color-mix(in srgb, ${colors.button.default.shade}, black 20%)`,
-        boxShadow: elevations.hover,
-    },
-    '&:focus-visible': {
-        backgroundColor: `color-mix(in srgb, ${colors.button.default.shade}, black 20%)`,
-        outline: `2px solid ${colors.focus.regular.outer}`,
-        boxShadow: [globalFocusShadow, elevations.hover].join(','),
-    },
-    '&:hover': {
-        backgroundColor: `color-mix(in srgb, ${colors.button.default.shade}, black 20%)`,
-        boxShadow: elevations.hover,
-        '&:focus-visible': {
-            boxShadow: [globalFocusShadow, elevations.hover].join(','),
+const buttonStyles = {
+    borderRadius: '8px',
+    height: '44px',
+    minWidth: '44px',
+    padding: '0.625rem',
+    border: `1px solid ${colors.surface.gray[60]} !important`,
+    fontSize: { xs: '12px', sm: '14px' },
+    boxShadow: 'none',
+    '&.MuiButton-containedPrimary': {
+        backgroundColor: colors.surface.blue[80],
+
+        '&:disabled': {
+            color: 'white',
         },
-    },
-    '&:active': {
-        backgroundColor: `color-mix(in srgb, ${colors.button.default.shade}, black 20%)`,
-        boxShadow: elevations.pressed,
-        '&:focus-visible': {
-            boxShadow: [globalFocusShadow, elevations.pressed].join(','),
-        },
-    },
-    '&:disabled': {
-        backgroundColor: '#EDEBE9',
-        boxShadow: 'none',
-        color: colors.type.regular.disabled,
     },
 };
 
-const SecondaryButtonStyles = {
-    backgroundColor: 'white',
-    color: colors.type.regular.primary,
-    '&:focus': {
-        backgroundColor: '#F8F8F8',
-        boxShadow: elevations.hover,
-        color: colors.type.inverted,
-        border: 'none',
-    },
-    '&:focus-visible': {
-        backgroundColor: '#F8F8F8',
-        outline: `2px solid ${colors.focus.regular.outer}`,
-        boxShadow: [globalFocusShadow, elevations.hover].join(','),
-        color: colors.type.inverted,
-        border: 'none',
-    },
-    '&:hover': {
-        backgroundColor: '#F8F8F8',
-        boxShadow: elevations.hover,
-        color: colors.type.inverted,
-        border: 'none',
-        '&:focus-visible': {
-            boxShadow: [globalFocusShadow, elevations.hover].join(','),
-        },
-    },
-    '&:active': {
-        backgroundColor: '#F8F8F8',
-        boxShadow: elevations.pressed,
-        color: colors.type.inverted,
-        border: `2px solid ${colors.surface.gray[110]}`,
-        '&:focus-visible': {
-            boxShadow: [globalFocusShadow, elevations.pressed].join(','),
-        },
-    },
-    '&:disabled': {
-        backgroundColor: 'white',
-        boxShadow: 'none',
-        color: colors.type.regular.disabled,
-    },
+interface PaginationProps {
+    page: number;
+    count: number;
+    onChange: (event: React.ChangeEvent<unknown>, page: number) => void | undefined;
+}
+
+/**
+ * A custom pagination component.
+ * It includes icon + text previous/next buttons, <page> of <count> summary, and custom page number display logic.
+ * Custom styling and conditional mobile rendering are also utilized.
+ * @param {PaginationProps} props - Props to pass to the pagination component.
+ * @param {number} props.page - The currently selected page of the pagination.
+ * @param {number} props.count - The total number of pages for the pagination.
+ * @param {function} props.onChange - The propagating onChange event that triggers the onChange callback function in the parent component.
+ * @returns {JSX.Element | null}
+ */
+export const Pagination = (props: PaginationProps) => {
+    const { onChange, page, count } = props;
+    if (!page || !Number.isFinite(page) || !count || !Number.isFinite(count) || !onChange) return null;
+
+    const { t: translate } = useAppTranslation();
+    const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'), { noSsr: true });
+
+    return (
+        <Grid container alignItems="center" gap={1} flexDirection={{ xs: 'column', sm: 'row' }}>
+            <Grid container component="nav" alignItems="center" flexWrap="nowrap" gap={{ xs: 0.5, md: 1 }}>
+                <Button
+                    aria-label="Go to previous page"
+                    sx={{ ...buttonStyles, p: isMobile ? '0 1rem' : '0 1.5rem' }}
+                    startIcon={isMobile ? null : <FontAwesomeIcon icon={faChevronLeft} />}
+                    disabled={page === 1}
+                    onClick={(e) => onChange(e as unknown as React.ChangeEvent<unknown>, Math.max(1, page - 1))}
+                >
+                    {translate('common.paginationPrevious')}
+                </Button>
+
+                <PageList page={page} count={count} onChange={onChange} />
+
+                <Button
+                    aria-label="Go to next page"
+                    sx={{ ...buttonStyles, p: isMobile ? '0 1rem' : '0 1.5rem' }}
+                    endIcon={isMobile ? null : <FontAwesomeIcon icon={faChevronRight} />}
+                    disabled={page === count}
+                    onClick={(e) => onChange(e as unknown as React.ChangeEvent<unknown>, Math.min(count, page + 1))}
+                >
+                    {translate('common.paginationNext')}
+                </Button>
+            </Grid>
+
+            <BodyText
+                aria-label={`Currently on page ${page} of ${count}`}
+                p="8px"
+                fontWeight={400}
+                color={colors.surface.gray[80]}
+            >
+                {`${translate('common.paginationPage')} ${page} ${translate('common.paginationOf')} ${count}`}
+            </BodyText>
+        </Grid>
+    );
 };
 
-const CustomPaginationItem = styled(PaginationItem)(({ selected }) => ({
-    ...(selected ? PrimaryButtonStyles : SecondaryButtonStyles),
-}));
+const PageList = (props: PaginationProps) => {
+    const { page, count, onChange } = props;
 
-export const Pagination: React.FC<PaginationProps> = (props) => {
-    return <MuiPagination {...props} renderItem={(item) => <CustomPaginationItem {...item} />} />;
+    // First page
+    const pageButtons = [1];
+    // Start ellipsis
+    if (page > 3) {
+        pageButtons.push(-2);
+    }
+    // Page before current page
+    if (page > 2 && page <= count) {
+        pageButtons.push(page - 1);
+    }
+    // Current page
+    if (page > 1 && page <= count) {
+        pageButtons.push(page);
+    }
+    // Page after current page
+    if (page > 0 && page < count - 1) {
+        pageButtons.push(page + 1);
+    }
+    // End ellipsis
+    if (page < count - 2) {
+        pageButtons.push(-1);
+    }
+    // Last page
+    if (!pageButtons.includes(count)) {
+        pageButtons.push(count);
+    }
+
+    return (
+        <Grid container gap={0.5} alignItems="flex-end" flexWrap="nowrap">
+            {pageButtons.map((pb) => {
+                const isCurrentPage = pb === page;
+                if (pb > 0) {
+                    return (
+                        <Button
+                            aria-label={`Go to page ${pb}`}
+                            key={`page-${pb}`}
+                            disabled={isCurrentPage ? true : undefined}
+                            variant={isCurrentPage ? 'primary' : 'secondary'}
+                            sx={buttonStyles}
+                            onClick={(e) => onChange(e as unknown as React.ChangeEvent<unknown>, pb)}
+                        >
+                            {pb}
+                        </Button>
+                    );
+                }
+                return (
+                    <BodyText
+                        aria-hidden="true"
+                        key={pb === -2 ? 'startEllipsis' : 'endEllipsis'}
+                        style={{ padding: '0 4px' }}
+                    >
+                        ...
+                    </BodyText>
+                );
+            })}
+        </Grid>
+    );
 };
--- web/src/components/landing/ResultsArea.tsx
+++ web/src/components/landing/ResultsArea.tsx
@@ -10,7 +10,7 @@ import { LandingDataContext } from '.';
 import { Engagement } from 'models/engagement';
 import { updateSearchParams } from './utils';
 
-const TileBlock = () => {
+const ResultsArea = () => {
     const {
         engagements: engs,
         loadingEngagements,
@@ -93,74 +93,47 @@ const TileBlock = () => {
                 paddingLeft={0}
                 component="ul"
                 my={0}
+                mb="4.5rem"
                 aria-label={`Engagements list. ${count} results.`}
                 direction="row"
                 columnSpacing={5}
-                justifyContent={'space-between'}
+                justifyContent={{ xs: 'center', md: 'space-between' }}
                 rowSpacing={4}
                 size={12}
+                display="grid"
+                sx={{
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 320px))',
+                }}
             >
                 {engagements?.map((engagement) => (
                     <Grid
-                        component="li"
                         container
+                        component="li"
                         key={engagement.id}
-                        size="auto"
                         sx={{
-                            flexBasis: '320px',
                             alignItems: 'center',
                             justifyContent: 'center',
                             listStyleType: 'none',
+                            minWidth: '320px',
                         }}
                     >
-                        <Grid width="320px">
-                            <EngagementTile passedEngagement={engagement} engagementId={engagement.id} />
-                        </Grid>
+                        <EngagementTile passedEngagement={engagement} engagementId={engagement.id} />
                     </Grid>
                 ))}
-                {/* Add 3 empty grid items to make sure final row is left aligned */}
-                {Array(3)
-                    .fill(0)
-                    .map((_, index) => (
-                        <Grid
-                            component="li"
-                            key={`empty-${index}`}
-                            size="auto"
-                            sx={{
-                                height: '1px',
-                                flexBasis: '320px',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                justifySelf: 'start',
-                                listStyleType: 'none',
-                            }}
-                        />
-                    ))}
-                <Grid
-                    size={12}
-                    container
-                    direction="row"
-                    alignItems={'center'}
-                    justifyContent="center"
-                    marginBottom="2em"
-                >
-                    <Grid>
-                        <Pagination
-                            defaultPage={1}
-                            page={Number(searchParams.get('page'))}
-                            count={Math.ceil(count / Number(searchParams.get('size')))}
-                            color="primary"
-                            showFirstButton
-                            showLastButton
-                            onChange={(_, pageNumber) =>
-                                setSearchParams(updateSearchParams({ page: pageNumber }, searchParams))
-                            }
-                        />
-                    </Grid>
+            </Grid>
+            <Grid size={12} container direction="row" alignItems={'center'} justifyContent="center" marginBottom="2em">
+                <Grid>
+                    <Pagination
+                        page={Number(searchParams.get('page')) || 1}
+                        count={Math.ceil(count / Number(searchParams.get('size') || 8))}
+                        onChange={(_, pageNumber) =>
+                            setSearchParams(updateSearchParams({ page: pageNumber }, searchParams))
+                        }
+                    />
                 </Grid>
             </Grid>
         </LiveAnnouncer>
     );
 };
 
-export default TileBlock;
+export default ResultsArea;
