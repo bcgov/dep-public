5006e12bf0975321b45b78bcead9a9179b459ca7
NatSquared <nat.k.weiland@gmail.com>
2026-08-19 10:49:21 -0700

    Implement suggestions from UX review

--- web/src/components/common/dateHelper.tsx
+++ web/src/components/common/dateHelper.tsx
@@ -29,3 +29,31 @@ export const formatToUTC = (date: Dayjs | string, formatString = 'YYYY-MM-DD HH:
         return '';
     }
 };
+
+export const formatRelative = (date: Dayjs | string) => {
+    const intervals = [
+        { label: 'year', seconds: 31536000 },
+        { label: 'month', seconds: 2592000 },
+        { label: 'day', seconds: 86400 },
+        { label: 'hour', seconds: 3600 },
+        { label: 'minute', seconds: 60 },
+        { label: 'second', seconds: 1 },
+    ];
+
+    const now = convertToPacific(dayjs().toString());
+    const targetDate = convertToPacific(date.toString());
+    const secondsElapsed = now.diff(targetDate, 'second');
+    const isFuture = secondsElapsed < 0;
+
+    const absSecondsElapsed = Math.abs(secondsElapsed);
+
+    for (const interval of intervals) {
+        const count = Math.floor(absSecondsElapsed / interval.seconds);
+        const pluralS = count > 1 ? 's' : '';
+        if (count >= 1) {
+            return isFuture ? `in ${count} ${interval.label}${pluralS}` : `${count} ${interval.label}${pluralS} ago`;
+        }
+    }
+
+    return 'just now';
+};
